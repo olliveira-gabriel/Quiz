648556aad21a795d0f0b24f952df25b39eff43a6
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-07-03 17:16:33 -0300

    Update file.js

---
+++ Quiz/file.js
@@ -0,0 +1,28 @@
+let Pergunta1 = document.getElementById('Menu_pergunta1')
+let Pergunta2 = document.getElementById('Menu_pergunta2')
+let Pergunta3 = document.getElementById('Menu_pergunta3')
+let Resultado = document.getElementById('InputResultado')
+
+
+function Exibir_Resultado(){
+    if (Pergunta1.value == 2 || Pergunta2.value == 2 || Pergunta3.value == 2){
+        alert("Selecione todas as questoes")
+    }
+    else{
+        alert("Quiz Finalizado! Confira sua pontuação")
+    }
+
+    
+    Resultado.value = (Number(Pergunta1.value) + Number(Pergunta2.value) + Number(Pergunta3.value))
+
+    if(Resultado.value == 3){
+        alert("Parabéns você acertou todas as perguntas!")
+    }
+    else{
+        Resultado.value
+    }
+
+    
+    
+
+}
